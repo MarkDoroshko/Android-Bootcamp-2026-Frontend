533b7327b1db9fa129366ef43e79fc884e12d773
Mark Doroshko <markdoroshko@yandex.ru>
2026-02-02 14:24:48 +0300

    Реализация экрана приглашений.

--- app/src/main/java/ru/sicampus/bootcamp2026/presentation/MainActivity.kt
+++ app/src/main/java/ru/sicampus/bootcamp2026/presentation/MainActivity.kt
@@ -1,18 +1,16 @@
 package ru.sicampus.bootcamp2026.presentation
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
+import androidx.annotation.RequiresApi
 import ru.sicampus.bootcamp2026.presentation.screen.creating_meeting.CreatingMeetingScreen
-import ru.sicampus.bootcamp2026.presentation.screen.registration.RegistrationScreen
 import ru.sicampus.bootcamp2026.presentation.ui.theme.AndroidBootcamp2026FrontendTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,13 +18,10 @@ class MainActivity : ComponentActivity() {
             AndroidBootcamp2026FrontendTheme(
                 darkTheme = false
             ) {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    CreatingMeetingScreen { }(
-                        modifier = Modifier.padding(innerPadding),
-                        onNavigateToAuthorization = {},
-                        onFinished = {}
-                    )
-                }
+                CreatingMeetingScreen(
+                    onBackClick = {},
+                    onFinished = {}
+                )
             }
         }
     }
